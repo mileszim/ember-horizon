26f66e7fc65f98f579c0e06540a13ffb8b7c4e77
Miles Zimmerman <miles@maztek.io>
2016-05-19 01:03:36 -0700

    Fixing the blueprint

--- blueprints/ember-horizon/index.js
+++ blueprints/ember-horizon/index.js
@@ -5,12 +5,18 @@ var EOL = require('os').EOL;
 module.exports = {
   description: ''
 
-  afterInstall(options) {
-    return this.addPackageToProject('@horizon/client', '1.0.1');
-  },
+  afterInstall: function(options) {
+    var self = this;
+    var host = 'localhost:8181';
 
-  addToConfig(key, value) {
-    var options = { after: '    environment: environment,' + EOL };
-    return this.insertIntoFile('config/environment.js', '    ' + key + ': ' + value + ',', options);
+    return this.addPackageToProject('@horizon/client', '1.0.1')
+    .then(function() {
+      var conf = "    horizon: {" + EOL + "      host: '" + host + "'" + EOL + "    },";
+      var opts = { after: '    environment: environment,' + EOL };
+      return self.insertIntoFile('config/environment.js', conf, opts);
+    })
+    .then(function() {
+      console.log("ember-horizon has been installed. Please configure your horizon connection in 'config/environment.js'.")
+    });
   }
 };
